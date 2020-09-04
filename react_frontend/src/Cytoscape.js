52876baa3027b6313ad0554010411d7b9bd5fef2
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-09-04 17:54:22 +0900

    fix position error of graph

--- react_frontend/src/Cytoscape.js
+++ react_frontend/src/Cytoscape.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
-import Container from '@material-ui/core/Container';
 import { withStyles } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const styles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
   },
 }));
 
@@ -19,18 +25,19 @@ class Cytoscape extends React.Component {
     render() {
       const { classes } = this.props;
       const elements = [
-        { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-        { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+        { data: { id: 'one', label: 'Node 1' }, position: { x: 100, y: 300 } },
+        { data: { id: 'two', label: 'Node 2' }, position: { x: 500, y: 300 } },
         { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
      ];
         return (
-          <Container maxWidth="lg" className={classes.container}>
-            <CytoscapeComponent 
+          <Grid item xs={12}>
+          <CytoscapeComponent 
               elements={elements} 
-              style={{ width: '600px', height: '600px'}}
               cy={(cy) => {this.cy = cy}}
-            />
-          </Container>
+              style = {{height:'600px'}}
+              className={classes.paper}
+              />
+          </Grid>
 
         )
     }
