625af0fb832b0fcfc5ce68c08fe767f2ccb839d6
yuichiroaoki <yuichiroaoki27@gmail.com>
2020-09-15 10:29:57 +0900

    update schema

--- responder_backend/database.py
+++ responder_backend/database.py
@@ -4,7 +4,7 @@ data = {}
 def setup():
     global data
 
-    from schema import NodeLabel, NodeEdges
+    from schema import NodeLabel, NodeEdges, EdgeList
 
     node1 = NodeLabel(id="1", label="Node1")
     node2 = NodeLabel(id="2", label="Node2")
@@ -15,12 +15,18 @@ def setup():
     node7 = NodeLabel(id="7", label="Node7")
     node8 = NodeLabel(id="8", label="Node8")
 
-    edge1 = NodeEdges(source="1", target="2", weight=1.2, rank=1)
+    edge1 = NodeEdges(source="1", target="2", weight=3.2, rank=1)
     edge2 = NodeEdges(source="1", target="3", weight=4.2, rank=1)
     edge3 = NodeEdges(source="2", target="5", weight=5.2, rank=1)
     edge4 = NodeEdges(source="3", target="6", weight=2.2, rank=1)
     edge5 = NodeEdges(source="3", target="5", weight=1.2, rank=2)
-    edge6 = NodeEdges(source="3", target="6", weight=1.2, rank=1)
+    edge6 = NodeEdges(source="3", target="6", weight=3.2, rank=1)
+    edge7 = NodeEdges(source="1", target="6", weight=0.2, rank=2)
+
+    edgeList1 = EdgeList(edge_list=["1", "2", "7"])
+    edgeList2 = EdgeList(edge_list=["3"])
+    edgeList3 = EdgeList(edge_list=["4", "5", "6"])
+
     data = {
         "NodeLabel": {
             "1": node1,
@@ -39,6 +45,12 @@ def setup():
             "4": edge4,
             "5": edge5,
             "6": edge6,
+            "7": edge7
+        },
+        "EdgeList" : {
+            "1": edgeList1,
+            "2": edgeList2,
+            "3": edgeList3,
         }
     }
 
@@ -47,3 +59,9 @@ def get_label(_id):
 
 def get_edges(_id):
     return data["NodeEdges"][_id]    
+
+def get_edge_list(_id):
+    return data["EdgeList"][_id]   
+
+def get_all_edges():
+    return data["NodeEdges"]
--- responder_backend/schema.py
+++ responder_backend/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene import relay
 
-from database import get_edges, get_label
+from database import get_edges, get_label, get_edge_list, get_all_edges
 
 class NodeLabel(graphene.ObjectType):
     class Meta:
@@ -32,14 +32,32 @@ class NodeEdges(graphene.ObjectType):
     def resolve_target(self, info, **args):
         return [get_label(label) for label in self.target]
 
+class EdgeConnection(relay.Connection):
+    class Meta:
+        node = NodeEdges
+
+class EdgeList(graphene.ObjectType):
+    class Meta:
+        interfaces = (relay.Node,)
+
+    edge_list = relay.ConnectionField(
+        EdgeConnection, description="A list of edges"
+    )
+
+    def resolve_edge_list(self, info, **args):
+        return [get_edges(label) for label in self.edge_list]
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     edges = graphene.Field(NodeEdges, id=graphene.ID(required=True))
+    edge_list = graphene.Field(EdgeList, id=graphene.ID(required=True))
+    all_edges = graphene.Field(NodeEdges)
 
     def resolve_edges(root, info, id):
         return get_edges(id)
 
+    def resolve_edge_list(root, info, id):
+        return get_edge_list(id)
 
 
 schema = graphene.Schema(query=Query)
